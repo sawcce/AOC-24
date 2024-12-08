484b233ff350fa29a6ec481bb6c6abfb57933120
alexis <sawcce@proton.me>
2024-12-08 12:53:36 +0100

    first commit

---
+++ day1.hs
@@ -0,0 +1,16 @@
+import Data.List
+import Data.Sequence (mapWithIndex)
+import System.Win32 (COORD (yPos))
+
+main :: IO ()
+main = do
+  input <- readFile "inputs/day1.txt"
+  let locations = words input
+  let ids :: [Integer] = map read locations
+
+  let left = sort $ map snd $ filter (even . fst) (zip [0 ..] ids)
+  let right = sort $ map snd $ filter (odd . fst) (zip [0 ..] ids)
+
+  let answer = foldl f 0 (zip left right) where f a (l, r) = a + abs (l - r)
+
+  print answer
