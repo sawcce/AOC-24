364afc8ed056b98d908502c5ff3dfadded452b3c
alexis <sawcce@proton.me>
2024-12-08 17:47:24 +0100

    Added part 2

--- day1.hs
+++ day1.hs
@@ -4,11 +4,13 @@ main :: IO ()
 main = do
   input <- readFile "inputs/day1.txt"
   let locations = words input
-  let ids :: [Integer] = map read locations
+  let ids :: [Int] = map read locations
 
   let left = sort $ map snd $ filter (even . fst) $ zip [0 ..] ids
   let right = sort $ map snd $ filter (odd . fst) $ zip [0 ..] ids
 
-  let answer = foldl f 0 (zip left right) where f a (l, r) = a + abs (l - r)
+  let distance = foldl f 0 (zip left right) where f a (l, r) = a + abs (l - r)
+  let similarityScore = sum $ map (\a -> a * length (filter (a ==) right)) left
 
-  print answer
+  print distance
+  print similarityScore
