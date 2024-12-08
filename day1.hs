7b70c252ce2c45f5a39bcc867ccf1bf6a161a72f
alexis <sawcce@proton.me>
2024-12-08 12:56:05 +0100

    Switched to $ for some extra style

--- day1.hs
+++ day1.hs
@@ -6,8 +6,8 @@ main = do
   let locations = words input
   let ids :: [Integer] = map read locations
 
-  let left = sort $ map snd $ filter (even . fst) (zip [0 ..] ids)
-  let right = sort $ map snd $ filter (odd . fst) (zip [0 ..] ids)
+  let left = sort $ map snd $ filter (even . fst) $ zip [0 ..] ids
+  let right = sort $ map snd $ filter (odd . fst) $ zip [0 ..] ids
 
   let answer = foldl f 0 (zip left right) where f a (l, r) = a + abs (l - r)
 
